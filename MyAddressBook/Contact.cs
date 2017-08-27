12f3a386eec80ad9bcb6563e301121aa0bd322c1
Alex Koch <lepiks@ukr.net>
2017-08-27 16:38:55 +0300

    first version

---
+++ MyAddressBook/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAddressBook
+{
+    class Contact
+    {
+        public Contact()
+        {
+        }
+
+        public string LName
+        { get; set; }
+
+        public string FName
+        { get; set; }
+
+        public string Birthday
+        { get; set; }
+
+        public string Mobile
+        { get; set; }
+     }
+
+}
---
+++ MyAddressBook/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAddressBook
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        protected List<Contact> contacts = new List<Contact>();
+
+        public List<Contact> Contacts
+        {
+            get { return contacts; }
+            set { contacts = value; }
+        }
+    }
+}
